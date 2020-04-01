05fda7334c799938b42c7ed8d435a6ff54f6f663
alpinelife37 <54160490+alpinelife37@users.noreply.github.com>
2020-04-01 11:45:37 -0700

    added basic html links to resource page

--- client/src/components/pages/Resources.js
+++ client/src/components/pages/Resources.js
@@ -1,20 +1,19 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Header, Segment } from "semantic-ui-react";
+import Links from "../resources/Links";
 
 class Dashboard extends Component {
   render() {
     return (
       <Segment style={{ minHeight: "400px" }}>
-      <Header as="h1" textAlign="center">
-        Resources
-      </Header>
-  </Segment>
+        <Header as="h1" textAlign="center">
+          Resources
+        </Header>
+        <Links />
+      </Segment>
     );
   }
 }
 
-
-
-export default connect(
-)(Dashboard);
+export default connect()(Dashboard);
