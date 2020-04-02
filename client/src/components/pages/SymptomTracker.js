7526cd946784f3c89d6d8021a45e81a8a63fc4bf
anil canbolat <alancturk1992@gmail.com>
2020-04-01 20:18:01 -0700

    added layout and symptoms to sypmtom tracker page

--- client/src/components/pages/SymptomTracker.js
+++ client/src/components/pages/SymptomTracker.js
@@ -1,18 +1,70 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import { Header, Segment } from "semantic-ui-react";
+import { Header, Segment, Container, Checkbox, Grid, Divider } from "semantic-ui-react";
 
 class SymptomTracker extends Component {
   render() {
-  //  const { user } = this.props.auth;  Incase anyone wants to display the users name they can call it by {user.name}
+    //  const { user } = this.props.auth;  Incase anyone wants to display the users name they can call it by {user.name}
 
     return (
-      <Segment style={{ minHeight: "400px" }}>
+      <div>
+        <Segment>
           <Header as="h1" textAlign="center">
             Symptom Tracker
           </Header>
-      </Segment>
+        </Segment>
+        <Segment>
+          <Container>
+            <Grid columns={2} relaxed='very'>
+              <Grid.Column >
+                <Header as="h3" textAlign='center'>
+                  Mark your symptoms below
+                </Header>
+                <Divider/>
+                <p>
+                  <Checkbox label='Fever' />
+                </p>
+                <p>
+                  <Checkbox label='Cough' />
+                </p>
+                <p>
+                  <Checkbox label='Shortness of breath' />
+                </p>
+                <p>
+                  <Checkbox label='Headaches' />
+                </p>
+                <p>
+                  <Checkbox label='Fatigue' />
+                </p>
+                <p>
+                  <Checkbox label='Trouble breathing' />
+                </p>
+                <p>
+                  <Checkbox label='Persistent pain or pressure in the chest' />
+                </p>
+                <p>
+                  <Checkbox label='New confusion or inability to arouse' />
+                </p>
+                <p>
+                  <Checkbox label='Bluish lips or face' />
+                </p>
+              </Grid.Column>
+
+              <Grid.Column>
+              <Header as="h3" textAlign='center'>
+                  Recomendations
+                </Header>
+                <Divider/>
+                <p>
+
+                </p>
+              </Grid.Column>
+            </Grid>
+            <Divider vertical />
+          </Container>
+        </Segment>
+      </div>
     );
   }
 }
