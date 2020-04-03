a55ebcc7a8460256c290c8d178770ceffc88c4fb
alpinelife37 <54160490+alpinelife37@users.noreply.github.com>
2020-04-02 19:15:15 -0700

    add Footer.js

---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <p>Copyright&copy;</p>
+      </div>
+    </footer>
+  );
+}
+export default Footer;
