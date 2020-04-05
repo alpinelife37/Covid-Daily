0da6462bdda417a6a4ad231a409d4675a58e2683
alpinelife37 <54160490+alpinelife37@users.noreply.github.com>
2020-04-05 13:46:01 -0700

    fixed misc console errors

--- client/src/components/pages/Resources.js
+++ client/src/components/pages/Resources.js
@@ -3,24 +3,25 @@ import { connect } from "react-redux";
 import { Header, Container, Divider } from "semantic-ui-react";
 import Links from "../resources/Links";
 import headerImg from "../resources/covid2.jpg";
-import ReactPlayer from "react-player"
-
+import ReactPlayer from "react-player";
 
 class Dashboard extends Component {
   render() {
     return (
       <Container style={{ minHeight: "400px" }}>
         <Header as="h1" textAlign="center">
-          <img src={headerImg} style={{ height: "200px", width: "350px", borderRadius: "100%" }} />
+          <img
+            src={headerImg}
+            alt="covidLogo"
+            style={{ height: "200px", width: "350px", borderRadius: "100%" }}
+          />
         </Header>
         <Divider />
         <Links />
         <Divider />
         <Container textAlign="center">
-          <div style={{display: "inline-block"}}>
-            <ReactPlayer
-              url="https://www.youtube.com/watch?v=BtN-goy9VOY"
-            />
+          <div style={{ display: "inline-block" }}>
+            <ReactPlayer url="https://www.youtube.com/watch?v=BtN-goy9VOY" />
           </div>
         </Container>
       </Container>
