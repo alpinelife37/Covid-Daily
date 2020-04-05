01f1d11c79a70e069b46b8106aa603ebe6d2dec9
alpinelife37 <54160490+alpinelife37@users.noreply.github.com>
2020-04-04 18:58:51 -0700

    added remote heroku connection to mlab in server.js

--- server.js
+++ server.js
@@ -11,15 +11,20 @@ const port = process.env.PORT || 5000;
 // Bodyparser middleware
 app.use(
   bodyParser.urlencoded({
-    extended: false
+    extended: false,
   })
 );
 app.use(bodyParser.json());
 
 mongoose
-  .connect(process.env.MONGODB_URI || "mongodb://localhost/users", { useNewUrlParser: true, useFindAndModify: false })
+  .connect(
+    process.env.MONGODB_URI ||
+      //"mongodb://localhost/users",
+      "mongodb://user:covid19@ds113866.mlab.com:13866/heroku_g4m022wz",
+    { useNewUrlParser: true, useFindAndModify: false }
+  )
   .then(() => console.log("MongoDB successfully connected"))
-  .catch(err => console.log(err));
+  .catch((err) => console.log(err));
 
 // Passport middleware
 app.use(passport.initialize());
@@ -30,6 +35,5 @@ require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
 app.use(require("./routes/api/symptoms"));
-
 
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
