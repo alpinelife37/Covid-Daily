7d1154edb205b1ad1cbec6de3bd33784c8d237f8
anil canbolat <alancturk1992@gmail.com>
2020-04-06 22:20:41 -0700

    styled dashboard

--- client/src/components/pages/Dashboard.js
+++ client/src/components/pages/Dashboard.js
@@ -18,84 +18,80 @@ class Dashboard extends Component {
   }
   handleChange(event) {
     event.preventDefault();
-    this.setState({ value: event.target.innerText }, () => 
-    console.log("Country Name: " + this.state.value));
- }
-  
-    render() {
-      const { user } = this.props.auth;
-
-      return (
-        <div id="componentBody" style={{ paddingTop: 25 }}>
-          <Grid responsive textAlign="center">
-            <Grid.Row>
-              <Header
-                as="h1"
-                style={{ marginLeft: 100, marginTop: 10, fontSize: 40 }}
-              >
-                Dashboard
-            </Header>
-              <Grid.Column floated="right" width={10}>
-                <Segment style={{minWidth: 300}}>
-                  <Grid>
-                    <Grid.Column width={5} floated="left">
-                      <DropdownCountrySearchSelection
-                        selectHandler={this.handleChange}
-                        value={this.state.value}
-                      />
-                    </Grid.Column>
-                    <Grid.Column verticalAlign="middle" width={5}>
-                      <Header>
-                        <Icon name="user" />
-                        {" " + user.name}
-                      </Header>
-                    </Grid.Column>
-                  </Grid>
-                </Segment>
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
-
-          <Divider />
-
-          <Grid columns={2} divided responsive="true" stackable>
-            <Grid.Row>
-              <Grid.Column width={4} className="componentcolumns">
-                <Header style={{ marginTop: 5 }} color="orange" as="h2">
-                  Latest News
-              </Header>
-                <Card style={{ width: "auto" }}>
-                  <News 
-                  
-                  />
-                </Card>
-              </Grid.Column>
-              <Grid.Column className="componentcolumns" id="recomendations">
-                <Header
-                  style={{ marginTop: 5 }}
-                  color="orange"
-                  textAlign="center"
-                  as="h2"
-                >
-                  Data:
-              </Header>
-                <CovidSearch
-                  value={this.state.value}
-                />
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
-        </div>
-      );
-    }
+    this.setState({ value: event.target.innerText }, () =>
+      console.log("Country Name: " + this.state.value));
   }
 
-  Dashboard.propTypes = {
-    auth: PropTypes.object.isRequired,
-  };
+  render() {
+    const { user } = this.props.auth;
 
-  const mapStateToProps = (state) => ({
-    auth: state.auth,
-  });
+    return (
+      <div id="componentBody" style={{ paddingTop: 25 }}>
+        <Grid responsive textAlign="center">
+          <Grid.Row>
+            <Header
+              as="h1"
+              style={{ marginLeft: 100, marginTop: 10, fontSize: 40 }}
+            >
+              Dashboard
+            </Header>
+            <Grid.Column verticalAlign="middle" floated="right" width={10}>
+              <Segment style={{ minWidth: 300 }}>
+                <Grid>
+                  <Header style={{ paddingTop: 22 }}>Select a Country: </Header>
+                  <Grid.Column style={{ paddingLeft: 0 }} width={5} floated="left">
 
-  export default connect(mapStateToProps)(Dashboard);
+                    <DropdownCountrySearchSelection
+                      selectHandler={this.handleChange}
+                      value={this.state.value}
+                    />
+                  </Grid.Column>
+                  <Grid.Column verticalAlign="middle" width={5}>
+                    <Header>
+                      <Icon name="user" />
+                      {" " + user.name}
+                    </Header>
+                  </Grid.Column>
+                </Grid>
+              </Segment>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+
+        <Divider />
+
+        <Grid columns={2} divided responsive="true" stackable>
+          <Grid.Row>
+            <Grid.Column width={4} className="componentcolumns">
+              <Header style={{ marginTop: 5 }} color="orange" as="h2">
+                Latest News
+              </Header>
+              <Card style={{ width: "auto" }}>
+                <News
+
+                />
+              </Card>
+            </Grid.Column>
+            <Grid.Column className="componentcolumns" id="recomendations">
+
+              <CovidSearch
+                value={this.state.value}
+              />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </div>
+    );
+  }
+}
+
+Dashboard.propTypes = {
+  auth: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+});
+
+export default connect(mapStateToProps)(Dashboard);
+
