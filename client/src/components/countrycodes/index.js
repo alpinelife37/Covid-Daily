c17b4b788d3b39e2911b837b355dc5728dbdda98
Sammye Jones <sajones@ucdavis.edu>
2020-04-08 16:42:13 -0700

    got state to update articles without breaking

---
+++ client/src/components/countrycodes/index.js
@@ -0,0 +1,67 @@
+
+
+function GetCountryCodes() {
+    const countryCodeArr = {
+        USA: "us",
+        UAE: "ae" ,
+        Argentina: "ar" ,
+        Austria: "at" ,
+        Australia: "au" ,
+        Belgium: "be" ,
+        Bulgaria: "bg" ,
+        Brazil: "br" ,
+        Canada: "ca" ,
+        Switzerland: "ch" ,
+        China: "cn" ,
+        Colombia: "co" ,
+        Cuba: "cu" ,
+        Czechia: "cz" ,
+        Germany: "de" ,
+        Egypt: "eg" ,
+        France: "fr" ,
+        UK: "gb" ,
+        Greece: "gr" ,
+        "Hong Kong":"hk" ,
+        Hungary: "hu" ,
+        Indonesia: "id" ,
+        Ireland: "ie" ,
+        Israel: "il" ,
+        India: "in" ,
+        Italy: "it" ,
+        Japan: "jp" ,
+        "S. Korea": "kr" ,
+        Lithuania: "lt" ,
+        Latvia: "lv" ,
+        Morocco: "ma" ,
+        Mexico: "mx" ,
+        Malaysia: "my" ,
+        Nigeria: "ng" ,
+        Netherlands: "nl" ,
+        Norway: "no" ,
+        "New Zealand": "nz" ,
+        Philippines: "ph" ,
+        Poland: "pl" ,
+        Portugal: "pt" ,
+        Romania: "ro" ,
+        Serbia: "rs" ,
+        Russia: "ru" ,
+        "Saudi Arabia": "sa" ,
+        Sweden: "se" ,
+        Singapore: "sg" ,
+        Slovenia: "si" ,
+        Slovakia: "sk" ,
+        Thailand: "th" ,
+        Turkey: "tr" ,
+        "Taiwan": "tw" ,
+        Ukraine: "ua" ,
+        Venezuela:"ve" ,
+        "South Africa": "za"
+    };
+    
+    return countryCodeArr;
+  }
+  
+  
+  
+  export default GetCountryCodes;
+  
--- client/src/components/news/index.js
+++ client/src/components/news/index.js
@@ -7,9 +7,8 @@ import {
     ButtonNext,
 } from "pure-react-carousel";
 import "pure-react-carousel/dist/react-carousel.es.css";
-import { Card, Header, Image, Divider } from "semantic-ui-react";
+import { Header, Image } from "semantic-ui-react";
 import CurrentDate from "../date";
-const axios = require("axios");
 
 const date = CurrentDate();
 
@@ -26,52 +25,43 @@ class News extends Component {
             country: "US",
         };
     }
-    // componentWillReceiveProps(nextProps) {
-    //     this.setState(
-    //         { country: nextProps.value }, () =>{
+    componentWillReceiveProps(props) {
+        this.setState(
+            { country: props.country }, () => {
+                let self = this;
+                var queryUrl = 'https://newsapi.org/v2/top-headlines?' +
+                    'q=corona&' +
+                    'country=' + this.state.country + '&' +
+                    'from=' + date + '&' +
+                    'sortBy=popularity&' +
+                    'apiKey=0d7e71ff7ddd480b83368a04bb626671';
 
-    //         var queryUrl = 'https://newsapi.org/v2/top-headlines?' +
-    //         'q=corona&' +
-    //         'country=' + this.state.country + '&' +
-    //         'from=' + date + '&' +
-    //         'sortBy=popularity&' +
-    //         'apiKey=0d7e71ff7ddd480b83368a04bb626671';
+                // Powered by news API
+                // API key 0d7e71ff7ddd480b83368a04bb626671
+                var req = new Request(queryUrl);
 
-    //         // Powered by news API
-    //         // API key 0d7e71ff7ddd480b83368a04bb626671
-    //         var req = new Request(queryUrl);
+                fetch(req).then(function (response) {
+                    response.json().then((data) => {
 
-    //         fetch(req)
-    //             .then(function (response) {
-    //                 response.json().then(data => {
-    //                     // do something with your data
-    //                     //console.log(data.articles);
+                        console.log(data);
+                        const filteredArticles = data.articles.filter((article) => {
+                            return (
+                                article.title &&
+                                article.description &&
+                                article.author &&
+                                article.url &&
+                                article.urlToImage
+                            );
+                        });
 
-    //                     let article;
-    //                     let i;
-    //                     for (i = 0; i < data.articles.length; i++) {
-    //                         article = data.articles[i];
-    //                         //console.log(article);
-    //                         if (article.title && article.description && article.author && article.url && article.urlToImage) {
-    //                             break;
-    //                         }
-    //                     };
-    //                     console.log(article.title);
+                        console.log(filteredArticles);
+                        self.setState({ filteredArticles }, () =>
+                            console.log(self.state));
+                    });
+                });
 
-    //                     this.setState({
-    //                         title: article.title,
-    //                         body: article.description,
-    //                         author: article.author,
-    //                         url: article.url,
-    //                         imgsrc: article.urlToImage
-    //                     });
-
-    //                 });
-
-    //             });
-
-    //         });
-    // }
+            });
+    }
 
     componentDidMount() {
         // an API call.
@@ -94,28 +84,7 @@ class News extends Component {
 
         fetch(req).then(function (response) {
             response.json().then((data) => {
-                // do something with your data
-                //console.log(data.articles);
 
-                // let article;
-                // let i;
-                // for (i = 0; i < data.articles.length; i++) {
-                //     article = data.articles[i];
-                //     //console.log(article);
-                //     if (article.title && article.description && article.author && article.url && article.urlToImage) {
-                //         break;
-                //     }
-                // };
-
-                // console.log(article.title);
-
-                // self.setState({
-                //     title: article.title,
-                //     body: article.description,
-                //     author: article.author,
-                //     url: article.url,
-                //     imgsrc: article.urlToImage
-                // });
 
                 const filteredArticles = data.articles.filter((article) => {
                     return (
@@ -126,8 +95,11 @@ class News extends Component {
                         article.urlToImage
                     );
                 });
-
-                self.setState({ filteredArticles });
+                
+                console.log(filteredArticles);
+                        self.setState({ filteredArticles }, () =>
+                            console.log(self.state));
+                //self.setState({ filteredArticles });
             });
         });
     }
@@ -176,9 +148,9 @@ class News extends Component {
                             );
                         })}
                     </Slider>
-                    <div style={{textAlign: "center"}}>
-                        <ButtonBack className="primary" style={{marginRight: 10, background: "#4169E1", color: "white"}}>Back</ButtonBack>
-                        <ButtonNext style={{background: "#4169E1", color: "white"}}>Next</ButtonNext>
+                    <div style={{ textAlign: "center" }}>
+                        <ButtonBack className="primary" style={{ marginRight: 10, background: "#4169E1", color: "white" }}>Back</ButtonBack>
+                        <ButtonNext style={{ background: "#4169E1", color: "white" }}>Next</ButtonNext>
                     </div>
                 </CarouselProvider>
             </div>
