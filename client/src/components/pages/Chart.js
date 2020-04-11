58fbea063a81b585d7affc9c32ee2f0c97cb694e
atcanbolat <kangalcanbolat@gmail.com>
2020-04-10 18:42:45 -0700

    added total history for chart button

--- client/src/components/pages/Chart.js
+++ client/src/components/pages/Chart.js
@@ -5,6 +5,10 @@ import { Button } from "semantic-ui-react";
 var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
 export default class Chart extends React.Component {
+  constructor() {
+		super();
+		this.getWeeklyData = this.getWeeklyData.bind(this);
+	}
   state = {
     data: [],
     countryname: "USA"
@@ -19,25 +23,33 @@ export default class Chart extends React.Component {
   }
 
   getWeeklyData() {
-    axios({
-      url:
-        "https://coronavirus-monitor.p.rapidapi.com/coronavirus/cases_by_particular_country.php",
-      method: "GET",
-      headers: {
-        "x-rapidapi-host": "coronavirus-monitor.p.rapidapi.com",
-        "x-rapidapi-key": "1e604574f3msh5f2b072afbe3555p1f4ebajsn89dc88914d1d",
-      },"params":{
-        "country": this.state.countryname 
-        }
-    }).then((response) => {
-      console.log(response)
-      this.setState({data: response.data.stat_by_country});
-    });
+          this.setState({data: []})
+       
+            axios({
+              url:
+                "https://coronavirus-monitor.p.rapidapi.com/coronavirus/cases_by_particular_country.php",
+              method: "GET",
+              headers: {
+                "x-rapidapi-host": "coronavirus-monitor.p.rapidapi.com",
+                "x-rapidapi-key": "1e604574f3msh5f2b072afbe3555p1f4ebajsn89dc88914d1d",
+              },"params":{
+                "country": this.state.countryname 
+                }
+            }).then((response) => {
+              console.log(this.state.countryname)
+              console.log(response)
+              response.data.stat_by_country.forEach((caseCount, i) => {
+                this.state.data.push({ x: Number(i), y: parseInt(caseCount.total_cases.replace(/,/g, ''))});
+                console.log(this.state.data)
+              });
+              this.chart.render();
+            })
+
   };
 
   componentWillReceiveProps(dayrecord) {
+    this.state.data = [];
     console.log(dayrecord);
-    this.setState({data: []});
     const dayLog = dayrecord.dayrecord.stat_by_country;
     const dataName = (dayLog[0].country_name);
     // console.log(dayLog[0].country_name);
@@ -66,14 +78,17 @@ export default class Chart extends React.Component {
         },
       ],
     };
+    console.log(options.data)
     return (
       <div>
         <CanvasJSChart
           options={options}
+          onRef={ref => this.chart = ref}
         />
         <Button
-        content="Weekly Data"
+        content="Total History"
         onClick={()=> this.getWeeklyData()}
+        onRef={ref => this.chart = ref}
         />
       </div>
     );
