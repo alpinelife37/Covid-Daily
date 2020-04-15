1682fd99b9db1a8824216eaae46fd62dab1e7e1c
Sammye Jones <sajones@ucdavis.edu>
2020-04-14 19:47:38 -0700

    fixed some grammar

--- client/src/utils/recomendationList.js
+++ client/src/utils/recomendationList.js
@@ -2,27 +2,27 @@ export default {
   none: {
     id: "Low chance that you have COVID-19",
     color: "green",
-    contenthead: "Looks like you dont have any symptoms!",
+    contenthead: "Looks like you don't have any symptoms!",
     contentbody:
-      "Guess what? You could still be infected and not showing any symptoms at all! Its best to still practice social distancing, and to follow your states or communities guidelines."
+      "Guess what? You could still be infected and not show any symptoms at all! It's best to still practice social distancing, and to follow your state's or community's guidelines."
   },
   low: {
     id: "Warning!",
     color: "yellow",
-    contenthead: "Warning, you may be a-symptomatic!",
+    contenthead: "Warning, you may be asymptomatic!",
     contentbody:
       "Please try to quarantine yourself if you can."
   },
   medium: {
     id: "ALERT!",
     color: "orange",
-    contenthead: "Alert! Theirs a very high chance you have the virus.",
+    contenthead: "Alert! There is a very high chance that you have the virus.",
     contentbody: "Please get tested as soon as possible!"
   },
   high: {
     id: "DANGER!",
     color: "red",
     contenthead: "Danger! You need professional medical care.",
-    contentbody: "Your body is having trouble recovering, seek immediate care"
+    contentbody: "Your body is having trouble recovering, seek immediate care."
   }
 };
